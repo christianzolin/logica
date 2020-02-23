f6ce4db9730b932eb6a07f1d0bbd97262b4b55e2
christianzolin <christian.koffer@gmail.com>
2020-02-23 10:42:16 -0300

    verificar se numeros são iguais, e verificar se numeros estão entre 1 e 100

--- atividade-4-2.js
+++ atividade-4-2.js
@@ -1,12 +1,26 @@
-var  user = require ('readline-sync');
+// Escreva um programa que pedirá para o usuário dois números entre 1 e 100. 
+// O programa deve, então, iterar de um em um do menor pro maior,
+//  sempre imprimindo o número na tela do usuário. Entretanto, caso o número seja divisível por 7, 
+//  o programa deve ao invés disso imprimir a palavra “Ping”. Caso o número seja divisível por 5, 
+//  o programa deve imprimir “Pong”. Caso o número seja divisível por 7 e 5, o programa deveria imprimir a 
+//  mensagem “Ping Pong”. Dica: Neste exercício vocês talvez precisem fazer uso do recurso
+//   “user.questionInt()” que transforma o conteúdo de um input do usuário de string para inteiro.
 
+
+var user = require ('readline-sync');
 var num1 = user.questionInt("Insira um numero entre 1 e 100: ")
-var num2 = user.questionInt("Insira um numero entre 1 e 100: ")
-
-//while(num1 >= 1 && num1 <= 100 & num2 >= 1 && num2 <= 100)
-
-  
-while(num1 <= num2){
+var num2 = user.questionInt("Insira um outro numero entre 1 e 100: ")
+    while(num1 < 1 || num1 > 100 && num2 < 1 || num2 > 100){
+    console.log("Numeros inseridos estão incorretos: ")
+    var num1 = user.questionInt("Insira um numero entre 1 e 100: ")
+    var num2 = user.questionInt("Insira um outro numero entre 1 e 100: \n")
+    }
+    while(num1 == num2){
+    console.log("Numeros inseridos não podem ser iguais: ")
+    var num1 = user.questionInt("Insira um numero entre 1 e 100: ")
+    var num2 = user.questionInt("Insira um outro numero entre 1 e 100: \n")
+    }       
+    while(num1 <= num2){
     if(num1%7==0 && num1%5==0){
         console.log("PING PONG")
     }
