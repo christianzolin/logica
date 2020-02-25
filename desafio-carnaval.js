326649e1ac5fec0060d7e5fe49189b8c050436b0
christianzolin <christian.koffer@gmail.com>
2020-02-24 21:38:00 -0300

    Mudando array para objeto de array && deixando perguntas aleatorias && pulo de perguntas

--- desafio-carnaval.js
+++ desafio-carnaval.js
@@ -1,34 +1,106 @@
 var user = require('readline-sync');
 var premio = [1000, 5000, 10000, 25000, 50000, 100000, 200000, 300000, 500000, 1000000];
 var premioAcumu = 0;
-var perguntas = ['De quantos anos e constituido um seculo?\n','Qual e o coletivo de caes?\n','Qual e a maior floresta do planeta?\n',
-'Qual e o naipe do baralho que tem o desenho de coracao?\n', 'Qual atriz estrelou no filme: “A lagoa azul”?\n', 'Qual e o pais do tango?\n',
-'Que imperador pos fogo em Roma?','O que e um oboe?\n', 'Quantos quilates tem o ouro puro?','Qual oceano tem o maior volume de agua?'];
-var alterna = ['A) 50 \nB) 100 \nC) 1,000 \nD) 1,500 \n','A) Matilha \nB) Rebanho \nC) Cardume \nD) Manada \n',
-'A) Negra \nB) de Sherwood \nC) da Tijuca \nD) Amazonica \n','A) Ouros \nB) Paus \nC) Copas \nD) Espadas \n',
-'A) Alicia Silverstone \nB) Brooke Shields \nC) Julia Roberts \nD) Jessica Lange \n','A) Uruguai \nB) Argentina \nC) Paraguai \nD) Espanha \n',
-'A) Trajano \nB) Nero \nC) Brutus \nD) Caligula \n','A) Vulcao \nB) Comida \nC) Instrumento Musical \nD) Tribo indigena \n',
-'A) 18 \nB) 20 \nC) 24 \nD) 30 \n','A) Atlantico \nB) Pacifico\nC) Indico\nD) Artico \n'];
-var respCerta = ['b','a','d','c','b','b','b','c','c','b'];
+var perguntas = [
+    {   pergun: "De quantos anos e constituido um seculo?\n",
+        alternativas: "A) 50 \nB) 100 \nC) 1,000 \nD) 1,500 \n",
+        res:"b",
+        perfull: function () {
+            return this.pergun + this.alternativas;            
+        }
+    },
+    {   pergun: "Qual e o coletivo de caes?\n",
+        alternativas: "A) Matilha \nB) Rebanho \nC) Cardume \nD) Manada \n" ,
+        res: "a",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }  
+    },
+    {   pergun:"Qual e a maior floresta do planeta?\n",
+        alternativas:"A) Negra \nB) de Sherwood \nC) da Tijuca \nD) Amazonica \n" ,
+        res:"d",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }
+    },
+    {   pergun: "Qual e o naipe do baralho que tem o desenho de coracao?\n",
+        alternativas:"A) Ouros \nB) Paus \nC) Copas \nD) Espadas \n",
+        res: "c",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }
+    },
+    {   pergun: "Qual atriz estrelou no filme: “A lagoa azul”?\n",
+        alternativas: "A) Alicia Silverstone \nB) Brooke Shields \nC) Julia Roberts \nD) Jessica Lange \n" ,
+        res:"b",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }
+    },
+    {   pergun: "Qual e o pais do tango?\n",
+        alternativas: "A) Uruguai \nB) Argentina \nC) Paraguai \nD) Espanha \n" ,
+        res:"b",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }
+    },
+    {   pergun: "Que imperador pos fogo em Roma?",
+        alternativas: "A) Trajano \nB) Nero \nC) Brutus \nD) Caligula \n" ,
+        res :"b",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }
+    },
+    {   pergun: "O que e um oboe?\n",
+        alternativas: "A) Vulcao \nB) Comida \nC) Instrumento Musical \nD) Tribo indigena \n" ,
+        res:"c",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }
+    },
+    {   pergun: "Quantos quilates tem o ouro puro?",
+        alternativas: "A) 18 \nB) 20 \nC) 24 \nD) 30 \n" ,
+        res:"c",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }
+    },
+    
+    {   pergun: "Qual oceano tem o maior volume de agua?",
+        alternativas: "A) Atlantico \nB) Pacifico\nC) Indico\nD) Artico \n" ,
+        res:"b",
+        perfull: function () {
+            return this.pergun + this.alternativas;
+        }
+    }
+]
+
 var play = true;
 var n = 0;
-
-
+var n2 = 0
+var pulo = 3
 
 while(play = true){
-    console.log("Comeca agora o show do milhao! ");
-    while(n<10){
-        
-        console.log("Essa pergunta vale "+premio[n]+" reais");
-        var respUsu = user.question("Pergunta: \n "+perguntas[n]+"\n"+alterna[n]);
-         if(respUsu != respCerta[n]){
+    console.log("Comeca agora o jogo de perguntas! ");
+    while(n2<10){
+        n = Math.floor(Math.random() * 10);
+        console.log("Essa pergunta vale "+premio[n2]+" reais");
+        var respUsu = user.question("Pergunta: \n "+perguntas[n].perfull()+"Zero para pular");
+        if(respUsu == "0" && pulo > 1){
+            n = Math.floor(Math.random() * 10);
+            var respUsu = user.question("Pergunta: \n "+perguntas[n].perfull()+"Zero");
+            pulo--;
+        }else {
+            console.log("Não pode mais pular")
+        }
+         if(respUsu != perguntas[n].res){
         console.log("Resposta errada!\n Infelizmente voce perdeu tudo!\n");
              break;
             }
-            if(respUsu == respCerta[n]){
-                premioAcumu = premio[n]
+            if(respUsu == perguntas[n].res){
+                premioAcumu = premio[n2]
                 console.log("Certa resposta! ");}
-                if(n < 9 && respUsu == respCerta[n]){
+                if(n2 < 9 && respUsu == perguntas[n].res){
                     var desistir = user.questionInt("Voce deseja desistir?\n 1 - Quero desistir!\n 2 - Quero continuar!\n ");
                     if(desistir == 1){
                         premioAcumu = premioAcumu/2;
@@ -39,10 +111,10 @@ while(play = true){
                 }else{
                     break
                 }
-                n++
+                n2++
             }
             
-            if(n==9 && respUsu == respCerta[n]){
+            if(n2==9 && respUsu == perguntas[n].res){
                 console.log("--------------------  PARABENS  --------------------\n");
                 console.log("----------  VOCE GANHOU UM MILHAO DE REAIS  ----------\n");
                 play = false;
@@ -53,6 +125,6 @@ while(play = true){
                 play = false;
                 break;
             }               
-        n = 0
+        n2 = 0
                     }
 console.log("Jogo encerrado! \n Obrigado por jogar! ");
